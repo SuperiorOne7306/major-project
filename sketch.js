183bad165b0f2ea9dafa09cf2511c3d676bc6e56
SuperiorOne7306 <58191376+SuperiorOne7306@users.noreply.github.com>
2021-02-10 14:11:18 -0600

    started experimenting with random encounters and health

--- sketch.js
+++ sketch.js
@@ -5,6 +5,8 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let whiteHealth = 200;
+let redHealth = 200;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -16,8 +18,41 @@ function setup() {
 function draw() {
   background(220);
   
-  fill("white")
-  rect(width/3, halfHeight, 100, 100);
-  fill("red");
-  rect(width/1.5, halfHeight, 100, 100);
+  displaySquares();
+}
+
+function keyPressed() {
+  if (key === "q") {
+    redHealth -= 20;
+  }
+  if (key === "a") {
+    whiteHealth -= 20;
+  }
+  console.log(whiteHealth);
+  console.log(redHealth);
+}
+
+function displaySquares() {
+  if (whiteHealth > 0) {
+    fill("white");
+    rect(width/3, halfHeight, 100, 100);
+  }
+  if (redHealth > 0) {
+    fill("red");
+    rect(width/1.5, halfHeight, 100, 100);
+  }
+}
+
+function randomEncounter() {
+  let numberOfEnemies;
+  let choice = random(0, 100);
+  if (choice >= 0 && choice < 50) {
+    numberOfEnemies = 1;
+  }
+  else if (choice >= 50 && choice < 75) {
+    numberOfEnemies = 2;
+  }
+  else if (choice >= 75 && choice <= 100) {
+    numberOfEnemies = 3;
+  }
 }
